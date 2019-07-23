db64f969ca22fe2c19a2af0d362ce9f7c5514976
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-23 15:17:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,7 +49,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, pledges.amount
 FROM projects
 INNER JOIN pledges
-ON projects.id = pledges.project_id
+ON projects.id = pledges.amount
 GROUP BY pledges.project_id
 HAVING projects.category = \"music\""
 end
