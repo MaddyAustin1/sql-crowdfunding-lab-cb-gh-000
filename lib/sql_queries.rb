b2bc2dc824e33eabbfb038f064a6305f7472affd
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-23 15:00:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,14 @@ ORDER BY users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, projects.funding_goal
+SUM(pledges.amount)
+FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+HAVING SUM(pledges.amount) > funding_goal
+GROUP BY projects.funding_goal
+ORDER BY "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
